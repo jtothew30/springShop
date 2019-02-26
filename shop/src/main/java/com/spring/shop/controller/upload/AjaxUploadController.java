52b932bf6911126be2e67a156d220057a74a26e7
jinwoo <jtothew@naver.com>
2019-02-26 16:23:50 +0900

    "brench1 새로생성" 이랑 합침

--- shop/src/main/java/com/spring/shop/controller/upload/AjaxUploadController.java
+++ shop/src/main/java/com/spring/shop/controller/upload/AjaxUploadController.java
@@ -1,5 +1,22 @@
 package com.spring.shop.controller.upload;
 
-public class AjaxUploadController {
+import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller
+public class AjaxUploadController {
+	private static final Logger logger = LoggerFactory.getLogger(AjaxUploadController.class);
+
+ 	//servlet-context.xml에 선언	
+	@Resource(name="uploadPath")
+	String uploadPath;
+
+ 	@RequestMapping(value="/upload/uploadAjax", method=RequestMethod.GET)
+	public void uploadAjax() {
+		// view/upload/uploadAjax.jsp로 이동
+	}
 }
